e1a880202f50b01265ee5991206a99b61e7f952a
jialinlu <jialinlu@amd.com>
2023-11-03 11:52:55 +0800

    Add another style of flowchart

--- README/plot.r
+++ README/plot.r
@@ -9,7 +9,7 @@ theme[['greenred']] = c('coral3','white','chartreuse4')
 #pdg<-rbind(rep(0,ncol(pdg)),pdg)
 #pdg<-cbind(rep(0,nrow(pdg)),pdg)
 
-pdf('o/pdg.pdf')
+pdf(commandArgs(trailingOnly=TRUE)[2])
 plot(pdg,col=c('coral3','white','chartreuse4'),family='mono')
 par(mar=diag(diag(4)*4))
 plot(pdg,col=theme$greenred,family='mono')
